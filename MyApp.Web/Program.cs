4feaa6776febb0a0ede91a5a35cf39388868b02a
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-06-28 17:32:43 +0700

    up

--- MyApp.Web/Program.cs
+++ MyApp.Web/Program.cs
@@ -9,7 +9,6 @@ using Quartz;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,9 +26,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
-
 app.UseAuthorization();
-
 
 //
 app.UseMiddleware<ExceptionMiddleware>();
@@ -49,9 +46,6 @@ using (var scope = app.Services.CreateScope())
 
 
 app.Run();
-
-
-
 
 static void MakeBuilder(WebApplicationBuilder builder)
 {

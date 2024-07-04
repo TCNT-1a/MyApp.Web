a6a68f23b0b3f9646e06777fe4fa2cf6f6d5d381
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-07-04 23:59:13 +0700

    up

--- MyApp.Web/Controllers/AccountController.cs
+++ MyApp.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Infrastructure.Data;
 using MyApp.Web.Models;
 using MyApp.Web.Models.Account;
 
@@ -6,13 +7,20 @@ namespace MyApp.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private BloggingContext _context;
+        public AccountController(BloggingContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Login(LoginModel loginModel,string returnUrl)
         {
-            var tokenProvider = new TokenProvider();
+            var tokenProvider = new TokenProvider(_context);
+            tokenProvider.LoginUser(loginModel.TenDangNhap, loginModel.MatKhau, true);
+
             ViewData["ReturnUrl"] = returnUrl;
             var model = new LoginModel();
             return View(model);

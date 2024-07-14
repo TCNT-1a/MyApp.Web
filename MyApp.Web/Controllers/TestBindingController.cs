36d4d8c695d7665f01a2e263e0cece58b9c5993e
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-07-14 21:24:02 +0700

    up

---
+++ MyApp.Web/Controllers/TestBindingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Web.Models.Account;
+
+namespace MyApp.Web.Controllers
+{
+    public class TestBindingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult NoBinding() {
+
+            if (Request?.Method =="POST")
+            {
+
+                var name = Request.Form["name"];
+                var birthyear = Request.Form["birthyear"];
+                ViewData["name"] = name;
+                ViewData["birthyear"] = birthyear;
+            }
+            return View(); 
+        }
+        public IActionResult SimpleBinding(string name, string birthyear)
+        {
+            ViewData["name"] = name;
+            ViewData["birthyear"] = birthyear;
+            return View();
+        }
+
+    }
+}

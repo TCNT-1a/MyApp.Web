7c541599060620ad09eba1a6a2a89a3c8b65adad
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-07-18 17:31:06 +0700

    up

--- MyApp.Web/Controllers/HomeController.cs
+++ MyApp.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ namespace MyApp.Web.Controllers
 {
     public static class SessionExtension
     {
-        public static void Set<T> (this ISession session, string key, T value)
+        public static void Set<T>(this ISession session, string key, T value)
         {
             var val = System.Text.Json.JsonSerializer.Serialize(value);
             session.SetString(key, val);
@@ -17,7 +17,6 @@ namespace MyApp.Web.Controllers
             else
                 return System.Text.Json.JsonSerializer.Deserialize<T>(value);
         }
-
     }
 
     public class HomeController : Controller
@@ -29,13 +28,18 @@ namespace MyApp.Web.Controllers
                 ViewBag.TrangThai = "Session chua khoi tao";
                 ViewBag.HoTen = "";
                 ViewBag.Tuoi = "";
-                CreateSession();
+                ViewBag.ThoiGian = "";
+                HttpContext.Session.Set<string>("hoten","Nguyen Van A");
+                HttpContext.Session.Set<int>("tuoi", 32);
+                HttpContext.Session.Set<DateTime>("thoigian", DateTime.Now.ToLocalTime());
+                //CreateSession();
             }
             else
             {
                 ViewBag.TrangThai = "Session duoc khoi tao";
                 ViewBag.HoTen = HttpContext.Session.GetString("hoten");
                 ViewBag.Tuoi = HttpContext.Session.GetInt32("tuoi");
+                ViewBag.ThoiGian = HttpContext.Session.GetInt32("thoigian");
             }
             return View();
         }

979471a53ead3944eb19e115261408b77c18762d
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-07-19 10:35:40 +0700

    up

--- MyApp.Web/Controllers/HomeController.cs
+++ MyApp.Web/Controllers/HomeController.cs
@@ -1,23 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MyApp.Web.Helper;
 namespace MyApp.Web.Controllers
 {
-    public static class SessionExtension
-    {
-        public static void Set<T>(this ISession session, string key, T value)
-        {
-            var val = System.Text.Json.JsonSerializer.Serialize(value);
-            session.SetString(key, val);
-        }
-        public static T Get<T>(this ISession session, string key)
-        {
-            var value = session.GetString(key);
-            if (value == null)
-                return default;
-            else
-                return System.Text.Json.JsonSerializer.Deserialize<T>(value);
-        }
-    }
+    
 
     public class HomeController : Controller
     {
@@ -32,21 +17,16 @@ namespace MyApp.Web.Controllers
                 HttpContext.Session.Set<string>("hoten","Nguyen Van A");
                 HttpContext.Session.Set<int>("tuoi", 32);
                 HttpContext.Session.Set<DateTime>("thoigian", DateTime.Now.ToLocalTime());
-                //CreateSession();
+                
             }
             else
             {
                 ViewBag.TrangThai = "Session duoc khoi tao";
-                ViewBag.HoTen = HttpContext.Session.GetString("hoten");
-                ViewBag.Tuoi = HttpContext.Session.GetInt32("tuoi");
-                ViewBag.ThoiGian = HttpContext.Session.GetInt32("thoigian");
+                ViewBag.HoTen = HttpContext.Session.Get<string>("hoten");
+                ViewBag.Tuoi = HttpContext.Session.Get<int>("tuoi");
+                ViewBag.ThoiGian = HttpContext.Session.Get<DateTime>("thoigian");
             }
             return View();
-        }
-        public void CreateSession()
-        {
-            HttpContext.Session.SetString("hoten","Nguyen Van A");
-            HttpContext.Session.SetInt32("tuoi",32);
         }
     }
 }

166da8d0320c8be22f5c62a025a1cb12b8a9dece
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-07-29 17:06:32 +0700

    up

--- MyApp.Infrastructure/Data/HeadingTag.cs
+++ MyApp.Infrastructure/Data/HeadingTag.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace MyApp.Infrastructure.Data
 {
-    public class HeadingTag: BaseEntity
+    public class HeadingTag : BaseEntity
     {
         public string Title { get; set; }
-        public string Description { get; set; }
-   
-        
+        public bool NoIndex { get; set; } 
+        public string Canonical { get; set; }
+        public string MetaDescription { get; set; }
     }
 }

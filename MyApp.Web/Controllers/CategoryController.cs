67a2e2461766f7daed955e38176b5ba7f17b6ecd
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-07-30 17:52:54 +0700

    up

---
+++ MyApp.Web/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Infrastructure.Data;
+using MyApp.Web.Controllers.Core;
+
+namespace MyApp.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private BloggingContext _context;
+        public CategoryController(BloggingContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
